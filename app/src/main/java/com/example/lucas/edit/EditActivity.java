0cc20342f3bfc7811287a2d70f6443c3ae7658f2
jlucas <joao.lucas@wit-software.com>
2019-02-16 16:50:53 +0000

    Adicionado actions à edit activity

--- app/src/main/java/com/example/lucas/edit/EditActivity.java
+++ app/src/main/java/com/example/lucas/edit/EditActivity.java
@@ -4,6 +4,10 @@ import com.example.lucas.main.R;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -15,7 +19,36 @@ public class EditActivity extends AppCompatActivity {
         setUiComponents();
     }
 
-    private void setUiComponents() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_edit, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_undo:
+                //TODO
+                Toast.makeText(this, "undo", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_redo:
+                //TODO
+                Toast.makeText(this, "redo", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_save:
+                //TODO
+                Toast.makeText(this, "save", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setUiComponents() {
+        // TODO
     }
 }
